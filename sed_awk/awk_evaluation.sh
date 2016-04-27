c6d7572eb78ddf1c79ac1521f7a7965adfdb041d
lw <liangwei888lw@hotmail.com>
2016-04-27 15:47:53 +0800

    awk_evaluation.sh

--- sed_awk/awk_evaluation.sh
+++ sed_awk/awk_evaluation.sh
@@ -29,3 +29,31 @@
 3 7 3
 [root@localhost lw]# awk 'NR==1{a=$1;b=$2;c=$3}$1<a{a=$1}$2<b{b=$2}$3<c{c=$3}END{print a,b,c}' 3.txt
 0 2 0
+
+
+
+
+####################################
+案例
+
+去掉一个最高分，和一个最低分，然后求各位的平均分。
+
+
+[root@localhostshare]# cat 1.txt 
+zs 58 98 56 23 22
+
+ls 75 23 2
+
+wmz 78 89 56 23
+
+xxoo 0 2 56 45 56 65
+[root@localhostshare]# awk 'NF{sum=0;max=$2;min=$2;for(i=2;i<=NF;i++){if(max<$i)max=$i;if(min>$i)min=$i;sum+=$i};print $1,(sum-max-min)/(i-4)}' 1.txt 
+zs 45.6667
+ls 23
+wmz 67
+xxoo 39.75
+[root@localhostshare]# awk 'NF{min=$2;for(i=1;i++<NF;){if($i>max)max=$i;if($i<min)min=$i;sum+=$i}print $1,(sum-max-min)/(NF-3);max=min=sum=a}' 1.txt 
+zs 45.6667
+ls 23
+wmz 67
+xxoo 39.75
