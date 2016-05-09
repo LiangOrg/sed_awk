c30fcc8d95fb40e5b84fdf6e58adda714cf6232b
lw <liangwei888lw@hotmail.com>
2016-05-09 17:11:50 +0800

    sed_awk_each_lines.sh

--- sed_awk/sed_awk_each_lines.sh
+++ sed_awk/sed_awk_each_lines.sh
@@ -9,6 +9,19 @@ cccccccccccccc
 dddddddddddddd
 
 eeeeeeeeeeeeeeeee
+
+[root@liudehua ~]#awk '{print;getline<"11.txt";print}' 1.txt
+1111111
+aaaaaaaaaa
+222222
+bbbbbbbbbbbb
+333333333
+cccccccccccccc
+444444444
+dddddddddddddd
+
+
+
 [root@liudehua ~]# sed 'R 11.txt' 1.txt
 1111111
 aaaaaaaaaa
