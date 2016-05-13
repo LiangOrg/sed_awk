df665f7cd3d46e994e578bc7efb90c0c07a1d8fd
lw <liangwei888lw@hotmail.com>
2016-05-13 19:01:52 +0800

    sed_space

---
+++ sed_awk/change_1_4row.sh
@@ -0,0 +1,99 @@
+#需求，将第一行的内容与第四行的内容互相。
+
+[root@localhost ~]# cat 1.txt
+aa bb
+cc dd
+ cc ll
+  ll kk
+1 2
+#lk
+lllll
+[root@localhost ~]# awk 'NR==FNR{if(NR==1)a=$0;if(NR==4){b=$0}next}FNR==1{$0=b}FNR==4{$0=a}1' 1.txt 1.txt
+  ll kk
+cc dd
+ cc ll
+aa bb
+1 2
+#lk
+lllll
+[root@localhost ~]# sed -r ':a;H;4bb;d;ba;:b;g;s/\n([^\n]+)\n(.+)\n([^\n]+)/\3\n\2\n\1/;:c;n;bc' 1.txt
+  ll kk
+cc dd
+ cc ll
+aa bb
+1 2
+#lk
+lllll
+[root@localhost ~]# sed -r ':a;H;4bb;d;ba;:b;g;s/\n([^\n]+)\n(.+)\n([^\n]+)/\3\n\2\n\1/;:c;n;bc' 1.txt|md5sum
+90b61d3f4af9c3a066d262e29f89d1f0  -
+[root@localhost ~]# awk 'NR==FNR{if(NR==1)a=$0;if(NR==4){b=$0}next}FNR==1{$0=b}FNR==4{$0=a}1' 1.txt 1.txt|md5sum
+90b61d3f4af9c3a066d262e29f89d1f0  -
+
+
+#seq 10|sed -nr '{N;N;N;s/^([^\n]+)(.*\n)([^\n]+$)/\3\2\1/gp};:c;n;p;bc'
+4
+2
+3
+1
+5
+6
+7
+8
+9
+10
+
+#
+seq 10|sed -r '1{h;d};:a;H;4bb;d;ba;:b;g;s/^([^\n]+)(.*\n)([^\n]+$)/\3\2\1/;:c;n;bc'
+4
+2
+3
+1
+5
+6
+7
+8
+9
+10
+
+
+#将第5行内容和第15行内容互换
+
+[root@local_game_server2 test]# seq 20|sed -r '5{h;d};6,14{H;d};15{H;g;s#^([^\n]+)(.*\n)([^\n]+)$#\3\2\1#g}'
+1
+2
+3
+4
+15
+6
+7
+8
+9
+10
+11
+12
+13
+14
+5
+16
+17
+18
+19
+20
+
+注释：核心思想是让保持空间的值储存为：
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+然后将第一行和最后一行互换位置（eg： s#^([^\n]+)(.*\n)([^\n]+)$#\3\2\1#g ）
+
+只处理5,15行，其他行不管它就行，他会自己打印出来
+
+
---
+++ sed_awk/sed_space_awk.sh
@@ -0,0 +1,489 @@
+#sed空间模式
+
+（一）
+只打印匹配行的上一行
+
+#cat empnametitle.txt 
+KLSLAL
+XXXXXXXXXXO
+Doe
+CEO
+Jason Smith
+IT Manager
+Raj Reddy
+Sysadmin
+Anand Ram
+Developer
+Jane Miller
+Sales Manager
+
+
+需求：打印经理的名字
+
+分析：即匹配关键字"Manager",打印他的上一行
+
+
+
+1.
+[root@local_game_server2 test]# sed  -n '/Manager/!h;/Manager/{x;p}' empnametitle.txt 
+Jason Smith
+Jane Miller
+
+
+2.
+[root@local_game_server2 test]# sed   '/Manager/!{h;d};x' empnametitle.txt 
+Jason Smith
+Jane Miller
+
+
+3.
+[root@local_game_server2 test]# sed -n 'x;n;/Manager/{x;p}' empnametitle.txt 
+Jason Smith
+Jane Miller
+
+
+
+注意：方法三有bug（当文件是奇数行是，无效），最好的方法是第一种，简介明了，易懂！
+
+
+
+
+
+awk:方法也是简单明了
+
+
+
+[root@local_game_server2 test]# awk '/Manager/{print s}{s=$0}' empnametitle.txt 
+Jason Smith
+Jane Miller
+
+解析：s=$0就是指的是上一行，直接打印s即可。
+
+
+
+
+
+
+打印passwd中/^bin/ 的上一行
+
+[root@local_game_server2 test]# sed '3q' passwd
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/sbin/nologin
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+
+
+
+1.
+[root@local_game_server2 test]# sed '/^bin/!{h;d};x' passwd
+root:x:0:0:root:/root:/bin/bash
+
+2.
+[root@local_game_server2 test]# sed -n '/^bin/!h;/^bin/{x;p}' passwd
+root:x:0:0:root:/root:/bin/bash
+
+
+
+
+
+（二）
+打印匹配行和匹配行的上一行
+
+
+
+需求：打印职务（经理）和姓名，并以冒号隔开。
+
+#cat empnametitle.txt 
+KLSLAL
+XXXXXXXXXXO
+Doe
+CEO
+Jason Smith
+IT Manager
+Raj Reddy
+Sysadmin
+Anand Ram
+Developer
+Jane Miller
+Sales Manager
+
+
+分析：即打印匹配到"Manager"和他的上一行。
+
+
+
+1.
+[root@local_game_server2 test]# sed  -n '/Manager/!h;/Manager/{G;s/\n/\t:/gp}' empnametitle.txt 
+IT Manager	:Jason Smith
+Sales Manager	:Jane Miller
+
+
+2.
+[root@local_game_server2 test]# sed   '/Manager/!{h;d};/Manager/{G;s/\n/\t:/g}' empnametitle.txt 
+IT Manager	:Jason Smith
+Sales Manager	:Jane Miller
+
+3.
+[root@local_game_server2 test]# sed  -n '{x;n};/Manager/{G;s/\n/\t:/gp}' empnametitle.txt 
+IT Manager	:Jason Smith
+Sales Manager	:Jane Miller
+
+
+
+注意：方法三有bug（当文件是奇数行是，无效），最好的方法是第一种，简介明了，易懂！
+
+
+
+awk
+
+[root@local_game_server2 test]# awk '/Manager/{print $0 "\t:" s}{s=$0}' empnametitle.txt 
+IT Manager	:Jason Smith
+Sales Manager	:Jane Miller
+
+
+解析：s=$0就是指的是上一行,打印$0,s即可。
+
+
+
+（三）
+打印匹配行的上一行和匹配行
+
+
+
+打印关键字"3"的上一行和匹配行
+
+
+
+[root@local_game_server2 test]# seq 5|sed -n '/3/!{h};/3/{H;x;p}'
+2
+3
+[root@local_game_server2 test]# seq 5|sed '/3/!{h;d};/3/{H;x}'
+2
+3
+[root@local_game_server2 test]# seq 5|sed -n '/3/!{h};/3/{x;p;x;p}'
+2
+3
+
+
+awk
+
+[root@local_game_server2 test]# seq 5|awk '/3/{print s RS $0}{s=$0}'
+2
+3
+
+
+
+（四）
+打印匹配行和匹配行的下一行
+1.
+#seq 5|sed  -n '/3/{N;p}'
+3
+4
+
+
+
+
+2.
+#seq 5|sed -n '/3/{p;n;p}'
+3
+4
+
+
+awk
+
+seq 5|awk 's~/3/{print s RS $0}{s=$0}'
+3
+4
+
+
+
+(五)
+打印匹配行的下一行和匹配行
+1.
+#[root@local_game_server2 test]# seq 5|sed -n '/3/{h;n;p;x;p}'
+4
+3
+
+
+2.
+#[root@local_game_server2 test]# seq 5|sed -n '/3/{h;n;G;p}'
+4
+3
+
+
+awk
+
+[root@local_game_server2 test]# seq 5|awk 's~/3/{print $0 RS s}{s=$0}'
+4
+3
+
+
+
+
+
+
+(六)
+打印匹配行的上一行和匹配行的下一行
+
+
+
+打印passwd中/^bin/ 的上一行和下一行
+
+
+[root@local_game_server2 test]# sed '3q' passwd
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/sbin/nologin
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+
+
+
+
+1.
+[root@local_game_server2 test]# sed -n  '/^bin/!h;/^bin/{x;p;n;p}'  passwd
+root:x:0:0:root:/root:/bin/bash
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+
+2.
+[root@local_game_server2 test]# sed   '/^bin/!{h;d};/^bin/{x;n}'  passwd
+root:x:0:0:root:/root:/bin/bash
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+
+
+(七)
+
+打印匹配行的下一行和上一行
+
+
+[root@local_game_server2 test]# seq 5|sed -n -e  '/3/!{h;d};/3/{x;h;n;p;x;p}' 
+4
+2
+[root@local_game_server2 test]# seq 5|sed -n -e  '/3/!h;/3/{x;h;n;p;x;p}' 
+4
+2
+[root@local_game_server2 test]# seq 5|sed -n -e  '/3/!h;/3/{n;p;x;p}' 
+4
+2
+[root@local_game_server2 test]# 
+[root@local_game_server2 test]# seq 5|sed -n -e  '/3/!h;/3/{n;p;g;p}' 
+4
+2
+
+awk
+
+[root@local_game_server2 test]# seq 5|awk 'b~/3/{print $0 RS a}{a=b;b=$0}'
+4
+2
+
+
+
+
+
+（八）
+
+倒叙（tac）的效果
+[root@local_game_server2 test]# seq 10|tac
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+[root@local_game_server2 test]# seq 10|sed '1!G;h;$!d'
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+[root@local_game_server2 test]# seq 10|sed -n '1!G;h;$p'
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+[root@local_game_server2 test]# seq 10|sed -n -e '1{h;d}' -e 'G;h;$p'
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+[root@local_game_server2 test]# seq 10|sed -n '1{h;d};G;h;$p'
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+[root@local_game_server2 test]# seq 10|sed '1{h;d};G;h;$!d'
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+（九）
+去掉最后两行
+[root@local_game_server2 test]# seq 11|sed -n 'N;$!P;D'
+1
+2
+3
+4
+5
+6
+7
+8
+9
+[root@local_game_server2 test]# seq 10|sed -n 'N;$!P;D'
+1
+2
+3
+4
+5
+6
+7
+8
+[root@local_game_server2 test]# seq 10|sed 'N;$!P;$!D;$d'
+1
+2
+3
+4
+5
+6
+7
+8
+[root@local_game_server2 test]# seq 11|sed 'N;$!P;$!D;$d'
+1
+2
+3
+4
+5
+6
+7
+8
+9
+[root@local_game_server2 test]# seq 11|sed 'N;$!{P;D};$d'
+1
+2
+3
+4
+5
+6
+7
+8
+9
+（十）
+只要最后两行
+[root@local_game_server2 test]# seq 10|sed '$!{h;d};H;x'
+9
+10
+[root@local_game_server2 test]# seq 11|sed '$!{h;d};H;x'
+10
+11
+[root@local_game_server2 test]# seq 10|sed 'N;$!D'
+9
+10
+[root@local_game_server2 test]# seq 11|sed 'N;$!D'
+10
+11
+（十一）
+不要最后10行
+（head -n -10）
+[root@local_game_server2 test]# seq 30|head -n -10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+[root@local_game_server2 test]# seq 30|sed -n -e :a -e '1,10!{P;N;D;};N;ba' 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+(十二)
+打印最后10行
+（tail -10）
+[root@local_game_server2 test]# seq 30|sed ':t;$q;N;11,$D;bt'
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+打印最后num行
+（tail -num）
+seq 30|sed ':t;$q;N;(num+1),$D;bt'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
