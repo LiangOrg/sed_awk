db4ca984d7b2ff316979cfd3373324eba69d406f
lw <liangwei888lw@hotmail.com>
2016-06-15 12:43:30 +0800

    all

--- sed_awk/sed_del_lines_up_and_down.sh
+++ sed_awk/sed_del_lines_up_and_down.sh
@@ -1,3 +1,4 @@
+
 [root@liudehua ~]# cat 1.txt
 aaaaaaa
 bbbbbbbbbb
@@ -31,3 +32,49 @@ bbbbbbbbbb
 xx00:8008:kkkkkkk
 ccccccccc
 dddddddddddd
+###################################################################
+1.需求将file文件中匹配到"xxoo"的行和它的上下两行（共5行）都删除。
+[root@local_game_server2 test]# cat file
+a
+b
+bb
+c
+d
+xxoo
+e
+f
+g
+kklll
+[root@local_game_server2 test]# sed  'N;N;/xxoo/{N;N;d}' file
+a
+b
+bb
+g
+kklll
+####################################################################
+2.需求，将file文件中匹配到"xxoo"的行和它的上下两行（共5行）的行首部加上"#"
+[root@local_game_server2 test]# cat file
+a
+b
+bb
+c
+d
+xxoo
+e
+f
+g
+kklll
+[root@local_game_server2 test]# sed 'N;N;/xxoo/{N;N;s/^/#/;s/\n/\n#/g}' file
+a
+b
+bb
+#c
+#d
+#xxoo
+#e
+#f
+g
+kklll
+
+
+awk '{a[NR]=$0;if(/xxoo/)b=NR}END{print "#"a[b-2],"#"a[b-1],"#"a[b],"#"a[b+1],"#"a[b+2]}' OFS="\n" file
